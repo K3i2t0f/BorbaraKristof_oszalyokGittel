f5ae26186eed20abbb5797ed72f69e9ce14cffb4
Kristóf Borbára <borbara.kristof@petrik.hu>
2025-02-07 15:55:25 +0100

    Karakter_Osztály

---
+++ OOPGyakorlas/Karakter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPGyakorlas
+{
+    internal class Karakter
+    {
+        private string nev;
+        private int szint;
+        private int eletero;
+        private int ero;
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Szint { get => szint; set => szint = value; }
+        public int Eletero { get => eletero; set => eletero = value; }
+        public int Ero { get => ero; set => ero = value; }
+
+        public Karakter(string nev, int szint, int eletero, int ero)
+        {
+            this.nev = nev;
+            this.szint = szint;
+            this.eletero = eletero;
+            this.ero = ero;
+        }
+
+        public Karakter(string nev, int ero)
+        {
+            this.nev = nev;
+            this.ero = ero;
+            szint = 1;
+            eletero = 100;
+        }
+
+        public void Tamadas(Karakter tamado)
+        {
+            tamado.Eletero -= ero;
+            Console.WriteLine($"{nev} támad {ero} erővel!");
+        }
+
+        public void Gyogyulas()
+        {
+            eletero += 10;
+        }
+
+        public void SzintLepes()
+        {
+            szint++;
+            eletero += 10;
+            ero += 5;
+        }
+
+        public override string ToString()
+        {
+            return $"{nev} - {szint} - {eletero} - {ero}";
+        }
+    }
+}
--- OOPGyakorlas/Program.cs
+++ OOPGyakorlas/Program.cs
@@ -7,21 +7,35 @@
             Konyv konyv1 = new("a Kis Herceg", "Antoine de Saint-Exupéry", 1943, 96, 2500);
             Konyv konyv2 = new("Ez még készül", "Én");
 
-            Film film1 = new Film("Star Wars", "George Lucas", 120, "sci-fi", true);
-            Film film2 = new Film("Ez még készül", "Én");
+            Film film1 = new("Star Wars", "George Lucas", 120, "sci-fi", true);
+            Film film2 = new("Ez még készül", "Én");
 
-            Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
+            Karakter Karakter1 = new("Karakter1", 10, 100, 20);
+            Karakter Karakter2 = new("Karakter2", 15, 100, 25);
+
 
             konyv1.Kedvezmeny(20);
             konyv1.Arnoveles(1000);
 
             Console.WriteLine("\n" + konyv1.ToString() + "\n" + konyv1.HanyOldalMaradt(20));
 
+            Console.WriteLine("-----------------------------------------------------------");
 
             film1.MufajModositas("sci-fi");
             film2.HosszNovelese(20);
 
             Console.WriteLine("\n" + film1.ToString() + "\n" + film2.ToString());
+
+            Console.WriteLine("-----------------------------------------------------------");
+
+            Karakter1.Tamadas(Karakter2);
+            Karakter2.Gyogyulas();
+
+            Console.WriteLine("\n" + Karakter1.ToString() + "\n" + Karakter2.ToString());
+
+            Console.WriteLine("-----------------------------------------------------------");
+
+
         }
     }
 }
