9f0ff8cb7bf28a869837d8dce172e36568e747ef
Kristóf Borbára <borbara.kristof@petrik.hu>
2025-02-07 15:28:49 +0100

    Film_Oszaly

---
+++ OOPGyakorlas/Film.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPGyakorlas
+{
+    internal class Film
+    {
+        private string cim;
+        private string rendezo;
+        private int hosszPercekben;
+        private string mufaj;
+        private bool megjelenjes;
+
+        public string Cim { get => cim; set => cim = value; }
+        public string Rendezo { get => rendezo; set => rendezo = value; }
+        public int HosszPercekben { get => hosszPercekben; set => hosszPercekben = value; }
+        public string Mufaj { get => mufaj; set => mufaj = value; }
+        public bool Megjelenjes { get => megjelenjes; set => megjelenjes = value; }
+
+        public Film(string cim, string rendezo, int hosszPercekben, string mufaj, bool megjelenjes)
+        {
+            this.cim = cim;
+            this.rendezo = rendezo;
+            this.hosszPercekben = hosszPercekben;
+            this.mufaj = mufaj;
+            this.megjelenjes = megjelenjes;
+        }
+
+        public Film(string cim, string rendezo)
+        {
+            this.cim = cim;
+            this.rendezo = rendezo;
+            hosszPercekben = 120;
+            mufaj = "ismeretlen";
+            megjelenjes = false;
+        }
+
+        public void Jatszas()
+        {
+            Console.WriteLine("A film elkezdődött!");
+        }
+
+        public string MufajModositas(string ujMufaj)
+        {
+            mufaj = ujMufaj;
+            return mufaj;
+        }
+
+        public void HosszNovelese(int percek)
+        {
+            if (percek > 0)
+            {
+                hosszPercekben += percek;
+            }
+            else
+            {
+                Console.WriteLine("Ezt a számot nem lehet hozzáadni!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{cim} - {rendezo} / {hosszPercekben} / {mufaj} - {megjelenjes}";
+        }
+    }
+}
--- OOPGyakorlas/Program.cs
+++ OOPGyakorlas/Program.cs
@@ -7,12 +7,21 @@
             Konyv konyv1 = new("a Kis Herceg", "Antoine de Saint-Exupéry", 1943, 96, 2500);
             Konyv konyv2 = new("Ez még készül", "Én");
 
+            Film film1 = new Film("Star Wars", "George Lucas", 120, "sci-fi", true);
+            Film film2 = new Film("Ez még készül", "Én");
+
             Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
 
             konyv1.Kedvezmeny(20);
             konyv1.Arnoveles(1000);
 
             Console.WriteLine("\n" + konyv1.ToString() + "\n" + konyv1.HanyOldalMaradt(20));
+
+
+            film1.MufajModositas("sci-fi");
+            film2.HosszNovelese(20);
+
+            Console.WriteLine("\n" + film1.ToString() + "\n" + film2.ToString());
         }
     }
 }
